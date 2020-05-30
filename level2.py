5441302ba27d66d0069d51c13154eb15794b7111
Pranav Doshetty <pranav.doshetty@gmail.com>
2020-05-30 10:18:27 +0530

    level2-part2

--- level2.py
+++ level2.py
@@ -3,26 +3,37 @@ b = int(input("Base "))
 
 k=len(n)
 
-x=''.join(sorted(n,reverse=True))
-y=''.join(sorted(n))
+for i in range(0,10):
+	x=''.join(sorted(n,reverse=True))
+	y=''.join(sorted(n))
 
-z=int(x,b)-int(y,b)
+	if b==10:
+		z=int(x)-int(y)
+	else:
+		z=int(x,b)-int(y,b)
 
-zcap=[]
+	zcap=[]
 
-while z>0 :
-	t=int(z%b)
-	zcap.append(t)
-	z=z//b
+	while z>0 :
+		t=int(z%b)
+		zcap.append(t)
+		z=z//b
 
-z=''.join(str(i) for i in zcap[::-1])
+	z=''.join(str(i) for i in zcap[::-1])
 
-zdash=int(t,3)
+	con=""
+	if len(z)<k:
+		for i in range(0,k-len(z)) :
+			con+="0"
+		z=con+z
 
-if zdash==n:
-	return 1
+	if z[:]==n[:] :
+		print(1)
+		break
 
-n=z[:]
+	print(z)
+
+	n=z[:]
 #got the end of first cycle of n
 #add condition to check if z is same length as n
 

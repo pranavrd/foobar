a33bb0791986e77e3bf98c94226c90a52f0fb2b7
Pranav Doshetty <pranav.doshetty@gmail.com>
2020-06-26 23:36:00 +0530

    level3_3 standard form of absorbing matrix

--- level3_3.py
+++ level3_3.py
@@ -9,6 +9,7 @@ t=[([0]*s) for i in range(s)]
 # 	print(m[i])
 for i in range(s):
 	if m[i].count(0)==len(m[i]):
+		m[i][i]=1
 		terms.append(i)
 		termlen+=1
 	else:
@@ -18,12 +19,20 @@ for i in range(s):
 	print(m[i])
 print("\n")
 
+k=0
+
+for i in range(s):
+	if m[i][i]==1:
+		t[k][k]=1
+		k+=1
+	
 for i in range(s):
 	for j in range(s):
-		for k in range(s):
-			if k not in terms:
-				t[i][j]+=m[i][k]*m[k][j]
+		if m[i][i]!=1:
+			t[i+termlen-1][j]=m[i][j]
 
 for i in range(s):
 	print(t[i])
-#print(terms,"\n",termlen)
+print("\n")
+
+print(terms,"\n",termlen)

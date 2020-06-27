886774e11365d2d69d55af7498ba196f9a715aba
Pranav Doshetty <pranav.doshetty@gmail.com>
2020-06-27 16:01:47 +0530

    level3_3 draft#1

--- level3_3.py
+++ level3_3.py
@@ -44,19 +44,16 @@ terms=[]
 termlen=0
 t=[([0]*s) for i in range(s)]
 
-# for i in range(s):
-# 	print(m[i])
 for i in range(s):
 	if m[i].count(0)==len(m[i]):
 		m[i][i]=1
-		terms.append(i)
 		termlen+=1
 	else:
 		m[i]=[j/sum(m[i]) for j in m[i]]
 
-for i in range(s):
-	print(m[i])
-print("\n")
+# for i in range(s):
+# 	print(m[i])
+# print("\n")
 
 k=0
 
@@ -68,7 +65,7 @@ for i in range(s):
 for i in range(s):
 	for j in range(s):
 		if m[i][i]!=1:
-			t[i+termlen][j]=m[i][j]
+			t[(i+termlen)%s][(j+termlen)%s]=m[i][j]
 
 r=[[0 for i in range(termlen)] for j in range(s-termlen)]
 q=[[0 for i in range(s-termlen)] for j in range(s-termlen)]
@@ -91,29 +88,47 @@ for i in range(s-termlen):
 		for k in range(s-termlen):
 			fr[i][j]+=f[i][k]*r[k][j]
 
-for i in range(s):
-	print(t[i])
-print("\n")
+# for i in range(s):
+# 	print(t[i])
+# print("\n")
 
-for i in range(s-termlen):
-	print(r[i])
-print("\n")
+# for i in range(s-termlen):
+# 	print(r[i])
+# print("\n")
 
-for i in range(s-termlen):
-	print(q[i])
-print("\n")
+# for i in range(s-termlen):
+# 	print(q[i])
+# print("\n")
 
-for i in range(s-termlen):
-	print(f[i])
-print("\n")
+# for i in range(s-termlen):
+# 	print(f[i])
+# print("\n")
 
-for i in range(s-termlen):
-	print(fr[i])
-print("\n")
+# for i in range(s-termlen):
+# 	print(fr[i])
+# print("\n")
 
-
+num=[]
+den=[]
 for i in fr[0]:
-	print(Fraction(i).limit_denominator())
+	stringo=str(Fraction(i).limit_denominator())
+	num.append(int(stringo.split("/")[0]))
+	try:
+		den.append(int(stringo.split("/")[1]))
+	except:
+		den.append(0)
 
-#print(np.dot(f,q))
-print(terms,"\n",termlen)
+maxden=max(den)
+
+for i in range(len(num)):
+	if num[i]==0:
+		terms.append(0)
+		continue
+	if den[i]<maxden:
+		terms.append(int(num[i]*maxden/den[i]))
+	else:
+		terms.append(num[i])
+
+terms.append(maxden)
+# print(num,"\n",den)
+print(terms)
